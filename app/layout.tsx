5aea01fa8cf2529d3d6df64eb53a3c7cfc9186ec
Dmitry Mytnikau <kfifa3@gmail.com>
2023-09-13 13:34:44 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ReactNode } from "react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'nimax',
+  description: 'Test for nimax',
+}
+
+export default function RootLayout({
+                                     children,
+                                   }: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en">
+    <body className={inter.className}>
+    <main className='grid place-content-center'>
+      {children}
+    </main>
+    </body>
+    </html>
+  )
+}
